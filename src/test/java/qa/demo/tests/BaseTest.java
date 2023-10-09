b0a8eece0e1281cb5ccf718be760c928e52dba24
Overloque <poludnicyn.99@mail.ru>
2023-10-10 01:56:58 +0500

    added allure for Web steps

--- src/test/java/qa/demo/tests/BaseTest.java
+++ src/test/java/qa/demo/tests/BaseTest.java
@@ -1,8 +1,11 @@
 package qa.demo.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import qa.demo.api.BookApi;
 import qa.demo.api.LoginApi;
 
@@ -16,5 +19,10 @@ public class BaseTest {
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.browserSize = "2560x1440";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
